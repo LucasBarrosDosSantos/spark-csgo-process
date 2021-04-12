4562ebd1222080bfb0846bdea58cde1f7da2cb6c
Lucas Barros <lucas2019.fullstack@gmail.com>
2021-04-12 18:49:55 -0300

    feature: relational player vs counter quantity of team

--- init.sh
+++ init.sh
@@ -1,1 +1,21 @@
-echo "iniciando o script de processamento"
+#!/bin/bash
+echo "iniciando o setup de processamento"
+echo ""
+archive=99
+# shellcheck disable=SC1057
+# shellcheck disable=SC1060
+# shellcheck disable=SC1073
+# shellcheck disable=SC2170
+while [ $archive -gt 1 ]; do
+  echo Olá, insira o número do arquivo você pretende gerar hoje ?
+  echo "1 - Sair do programa"
+  echo "2 - Relação de players x time"
+  read archive
+  if [ $archive -eq 2 ]; then
+    echo "Iniciando processamento do arquivo de players vs time"
+    ~/spark/spark-3.1.1-bin-hadoop2.7/bin/spark-submit --class org.apache.spark.examples.Spark_Pi --master local[8] quantityPlayersOfTeams.py
+  fi
+done
+# shellcheck disable=SC1073
+
+echo "Obrigado pela visita, volte assim que precisar de arquivos atualizados"
--- quantityPlayersOfTeams.py
+++ quantityPlayersOfTeams.py
@@ -1,3 +1,9 @@
 # Essa função processa a quantidade de jogadores a cada time de forma decrescente e jogando
 # a saida em um arquivo csv para uso em relatórios, gráficos ou ferramentas de analytics
+from pyspark import SparkContext, SparkConf
+from pyspark.shell import spark, sqlContext
+from pyspark.sql import SQLContext
 
+conf = SparkConf().setAppName("PySparkShell")
+df = spark.read.csv('/home/lucas/spark/players.csv', header=True)
+df = df.groupBy("team").count().orderBy("count", ascending=False).repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("teamVsPlayersNumbers")
